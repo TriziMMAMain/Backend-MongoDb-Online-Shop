13209f8fd17f62789f4e60c207e01b278cb4cdec
unknown <samparizonarp131212@gmail.com>
2023-06-11 16:49:36 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,8 +7,7 @@ const instrumentRouter = require('./routes/instrument-routes.js')
 
 const PORT = 3000
 // const URL = 'mongodb+srv://Rustam:rusqtam01q1@mongodbonlineshopvseins.r6glwty.mongodb.net/instrumentBox'
-const URL = "process.env.MONGODB_URI"
-    
+
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
@@ -20,7 +19,7 @@ app.listen(PORT, (err) => {
 })
 
 mongoose
-    .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then((res) => {
         console.log(`Connected to MongoDB`)
     })
