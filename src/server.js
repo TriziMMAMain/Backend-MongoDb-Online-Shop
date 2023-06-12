b5db6c7f1e573ef839fdb765c5036a369539eaa1
unknown <samparizonarp131212@gmail.com>
2023-06-12 21:24:00 +0300

    Update 21:23

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const cors = require('cors')
 const instrumentRouter = require('./routes/instrument-routes.js')
-
+const instrumentController = require('./constrollers/instrument-controller.js')
 
 const PORT = process.env.PORT || 4000
 const URL = 'mongodb+srv://Rustam:rusqtam01q1@mongodbonlineshopvseins.r6glwty.mongodb.net/instrumentBox'
@@ -14,6 +14,16 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(instrumentRouter)
+app.use(instrumentController)
+
+
+app.listen(PORT, (err) => {
+    if (!err) {
+        console.log(`listening port ${PORT}`)
+    } else {
+        console.log(err)
+    }
+})
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -21,13 +31,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
     .then((res) => {
         console.log("MongoDB connected ")
-        app.listen(PORT, (err) => {
-            if (!err) {
-                console.log(`listening port ${PORT}`)
-            } else {
-                console.log(err)
-            }
-        })
     })
     .catch((err) => {
         console.error("MongoDB connection error: ", err)
