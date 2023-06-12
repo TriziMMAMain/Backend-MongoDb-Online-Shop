657bf81ee8714e58d33a1cac87b40188f06bb52c
unknown <samparizonarp131212@gmail.com>
2023-06-12 13:52:08 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,10 +16,9 @@ app.use(bodyParser.json())
 app.use(instrumentRouter)
 
 
-mongoose
-    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,})
-    .then((res) => {
-        console.log(`Connected to MongoDB`)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log(`Connected to MongoDB`,)
     })
     .catch((err) => {
         console.log(`Db connection error ${err}`)
