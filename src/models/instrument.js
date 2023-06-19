bae4bed4c2344bccc01156c53401fb3d773c8ddf
unknown <samparizonarp131212@gmail.com>
2023-06-19 14:33:57 +0300

    Update instrument.js

--- src/models/instrument.js
+++ src/models/instrument.js
@@ -5,60 +5,48 @@ const Schema = mongoose.Schema
 const instrumentSchema = new Schema({
     "id": {
         type: Number,
-        required: false
     },
 
     "numberInList": {
         type: Number,
-        required: false
     },
     "type": {
         type: String,
-        required: false
     },
     "typeThis": {
         type: String,
-        required: false
     },
     "brand": {
         type: String,
-        required: false
     },
     "name": {
         type: String,
-        required: false
     },
     "city": {
         type: String,
-        required: false
     },
     "featureTop": {
         type: String,
-        required: false
     },
     "featureTopTitle": {
         type: [{
             featureTopTitleInfoTitle: String,
             featureTopTitleInfoText: String
         }],
-        required: false
     },
     "featureMiddle": {
         type: [{
             feature: String,
             featureValue: String
         }],
-        required: false
     },
     "featureDownArray": {
         type: [{
             featureDown: String
         }],
-        required: false
     },
     "availability": {
         type: String,
-        required: false
     },
     "avalibilitySecond": {
         type: Boolean,
@@ -66,25 +54,20 @@ const instrumentSchema = new Schema({
     },
     "imgTitle": {
         type: String,
-        required: false
     },
     "imgArray": {
         type: [{
             src: String
         }],
-        required: false
     },
     "price": {
         type: Number,
-        required: false
     },
     "orderSum": {
         type: Number,
-        required: false
     },
     "priceOrder": {
         type: Number,
-        required: false
     }
 }, { versionKey: false })
 
